9896dfb6a432cfeaeb944e4a9288e12969fbf893
Andres <dandres159@gmail.com>
2023-04-24 22:06:06 +0200

    Agregando nuevos comentarios para entender mejor el funcionamiento de la aplicacion

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,7 +9,10 @@ import { getGifs } from '../helpers/getGifs';
 export const GifGrid = ({ category }) => {
 
 
+    // Se crea una instancia del custom Hook que hemos creado que nos devolvera los gifs y un estado para el cargando de la pagina
     const { images, isLoading } = useFetchGifs( category );
+
+    
 
   return (
     <>
@@ -20,9 +23,13 @@ export const GifGrid = ({ category }) => {
 
         <div className='card-grid'>
             {/* Images.map */}
+            {/* Recorremos el arreglo de objeto, el cual tiene los gifs de la peticion asincrona en su interior
+                a cada uno de ellos se le asignara una key que sera el id de la imagen ademas se pasara las propiedades
+                de cada objeto al componente funcional   */}
             {
                 images.map( ( image ) =>(
                     <GifItem 
+                    
                         key={ image.id }
                         //Lo que se hace aqui es esparcir las propiedades, es util cuando se tienen muchas propiedades
                         { ...image}
